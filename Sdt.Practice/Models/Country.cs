0a0eb224c21fc3a4bb79c1d3983cb7887519f77d
陈海荣 <chenhairong@sunlight.bz>
2018-11-30 13:06:49 +0800

    添加项目文件。

---
+++ Sdt.Practice/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Sdt.Practice.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string EnglishName { get; set; }
+
+        public string ChineseName { get; set; }
+    }
+}
---
+++ Sdt.Practice/RestApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sdt.Practice.Models;
+
+namespace Sdt.Practice
+{
+    public class RestApiContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        public RestApiContext(DbContextOptions<RestApiContext> options) : base(options)
+        {
+
+        }
+    }
+}
